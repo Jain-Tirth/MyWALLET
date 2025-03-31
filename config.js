88ef45886b4b6289791b3fc9eb5b81cef93ccee2
Tirth Jain <jaintirth2705@gmail.com>
2025-03-31 10:51:52 +0530

    Added copy address functionality with copy button and styling

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 const appwriteConfig = {
-    endpoint: 'YOUR_APPWRITE_ENDPOINT',
-    projectId: 'YOUR_PROJECT_ID',
-    databaseId: 'YOUR_DATABASE_ID',
-    usersCollectionId: 'YOUR_USERS_COLLECTION_ID',
-    tokensCollectionId: 'YOUR_TOKEN_COLLECTION_ID',
-    accountsCollectionId: 'YOUR_ACCOUNT_COLLECTION_ID'
+    endpoint: 'https://cloud.appwrite.io/v1',
+    projectId: '67df04d700383e86f2ae',
+    databaseId: '67df05400001153d826f',
+    usersCollectionId: '67df0b9300296e8e5f51',
+    tokensCollectionId: '67e8ee5700364a4b2b58',
+    accountsCollectionId: '67e8ee090010fe7e2b42',
+    secretKey: 'standard_b4a6bf436e50d0073b0f74b1ccf562105877076bdbf304e6c076e9b67dafb75e488703e4fbfca501227758581524d5916271220d86301e819587b6f35f125dd58670c2de6296b1214172cd92bd356df1bca6d52c72fa0ed9c1750181a0d86407ec56e87e79cfefb968dc2296721011887f40fb2cae4c1ed4b397e57712d75e53'
 };
 
 module.exports = appwriteConfig; 
